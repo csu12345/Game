bb606dc750a9cf2f6f1b7119ea23f82e2004880a
csu12345 <aew.kalyan@gmail.com>
2015-10-19 18:29:38 +1100

    Initial code
    Initial code

---
+++ Dice.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+/**
+ *
+ * @author 11588087
+ */
+public class Dice {
+			
+	private DiceValue value;
+	
+	public Dice() {
+		value =  DiceValue.getRandom();
+	}
+	
+	public DiceValue getValue() {
+		return value;
+	}
+
+	public DiceValue roll() {
+		return DiceValue.getRandom();
+	}		
+	
+	public String toString() {
+		return value.toString();
+	}
+}
+
