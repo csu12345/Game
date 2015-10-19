55b98ddf18151daf328220ffc0f6a713f3ed302b
csu12345 <aew.kalyan@gmail.com>
2015-10-19 18:31:37 +1100

    Solved Spade bug
    Added another variable that would resolve enum value of spade variable

---
+++ DiceValue.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+/**
+ *
+ * @author 11588087
+ */
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public enum DiceValue {
+	CROWN, ANCHOR, HEART, DIAMOND, CLUB, SPADE,UNKNOWN;
+	
+	private static Random RANDOM = new Random();
+	
+	private static final Map<DiceValue, String> VALUE_REPR_MAP= new HashMap<DiceValue, String>();
+	static {
+		VALUE_REPR_MAP.put(DiceValue.CROWN, "Crown");
+		VALUE_REPR_MAP.put(DiceValue.ANCHOR, "Anchor");
+		VALUE_REPR_MAP.put(DiceValue.HEART, "Heart");
+		VALUE_REPR_MAP.put(DiceValue.DIAMOND, "Diamond");
+		VALUE_REPR_MAP.put(DiceValue.CLUB, "Club");
+		VALUE_REPR_MAP.put(DiceValue.SPADE, "Spade");
+                VALUE_REPR_MAP.put(DiceValue.UNKNOWN, "Unknown");
+	}
+	
+	public String toString(DiceValue value) {
+		return VALUE_REPR_MAP.get(value);
+	}
+	
+	public static DiceValue getRandom() {
+		int random = RANDOM.nextInt(DiceValue.UNKNOWN.ordinal());
+		return values()[random];
+	}
+	
+}
+
