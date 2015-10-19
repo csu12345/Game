10f41afbda6b9528a6fa5a9dd739e865e4127c26
csu12345 <aew.kalyan@gmail.com>
2015-10-19 18:41:11 +1100

    Solved Age Verification Bug
    Program will ask for player name and age, If player is above 18 he will
    proceed to game otherwise it terminates

---
+++ Main.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+/**
+ *
+ * @author 11588087
+ */
+import java.util.List;
+import java.util.Scanner;
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+            
+          String name;
+          int age;
+          Scanner scanner = new Scanner(System.in);
+           
+		
+	   BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+
+        Dice d1 = new Dice();
+        Dice d2 = new Dice();
+        Dice d3 = new Dice();
+        
+        System.out.println("Enter Player Name: ");
+        name = scanner.nextLine();
+        System.out.println("Enter Player Age: ");
+        age = scanner.nextInt();
+        
+        if(age >= 18)
+        {
+
+        Player player = new Player(name, 100);
+        Game game = new Game(d1, d2, d3);
+        List<DiceValue> cdv = game.getDiceValues();
+        
+
+        int totalWins = 0;
+        int totalLosses = 0;
+
+        while (true)
+        {
+            int winCount = 0;
+            int loseCount = 0;
+            
+            for (int i = 0; i < 100; i++)
+            {
+            	//String name = "Fred";
+            	int balance = 100;
+            	int limit = 0;
+                player = new Player(name, balance);
+                player.setLimit(limit);
+                int bet = 5;
+
+                System.out.println(String.format("Start Game %d: ", i));
+                System.out.println(String.format("%s starts with balance %d, limit %d", 
+                		player.getName(), player.getBalance(), player.getLimit()));
+
+                int turn = 0;
+                while (player.balanceExceedsLimitBy(bet) && player.getBalance() < 200)
+                {
+                    turn++;                    
+                	DiceValue pick = DiceValue.getRandom();
+                   
+                	System.out.printf("Turn %d: %s bet %d on %s\n",
+                			turn, player.getName(), bet, pick); 
+                	
+                	int winnings = game.playRound(player, pick, bet);
+                    cdv = game.getDiceValues();
+                    
+                    System.out.printf("Rolled %s, %s, %s\n",
+                    		cdv.get(0), cdv.get(1), cdv.get(2));
+                    
+                    if (winnings > 0) {
+	                    System.out.printf("%s won %d, balance now %d\n\n",
+	                    		player.getName(), winnings, player.getBalance());
+	                	winCount++; 
+                    }
+                    else {
+	                    System.out.printf("%s lost, balance now %d\n\n",
+	                    		player.getName(), player.getBalance());
+	                	loseCount++;
+                    }
+                    
+                } //while
+
+                System.out.print(String.format("%d turns later.\nEnd Game %d: ", turn, i));
+                System.out.println(String.format("%s now has balance %d\n", player.getName(), player.getBalance()));
+                
+            } //for
+            
+            System.out.println(String.format("Win count = %d, Lose Count = %d, %.2f", winCount, loseCount, (float) winCount/(winCount+loseCount)));
+            totalWins += winCount;
+            totalLosses += loseCount;
+
+            String ans = console.readLine();
+            if (ans.equals("q")) break;
+        } //while true
+        
+        System.out.println(String.format("Overall win rate = %.1f%%", (float)(totalWins * 100) / (totalWins + totalLosses)));
+	}
+        else
+        {
+            System.out.println("Player age under 18 cannot play ");
+        }
+        
+        }
+
+}
